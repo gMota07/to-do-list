3b795d3cc6e77fd9819855fe502887d3cb593ed1
Gabriel Mota <gabrielmotavalerio@gmail.com>
2026-03-19 22:00:49 -0300

    fix: integrar frontend no backend

--- backend/public/frontend/src/api/taskService.js
+++ backend/public/frontend/src/api/taskService.js
@@ -2,19 +2,19 @@ const BASE_URL = 'http://localhost:3333';
 
 export const taskService = {
   async findAll() {
-    const res = await fetch(`${BASE_URL}/task`);
+    const res = await fetch(`/task`);
     if (!res.ok) throw new Error('Erro ao buscar tarefas');
     return res.json();
   },
 
   async findByID(id) {
-    const res = await fetch(`${BASE_URL}/task/${id}`);
+    const res = await fetch(`/task/${id}`);
     if (!res.ok) throw new Error('Erro ao buscar tarefa');
     return res.json();
   },
 
   async create(data) {
-    const res = await fetch(`${BASE_URL}/task`, {
+    const res = await fetch(`/task`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(data)
@@ -24,7 +24,7 @@ export const taskService = {
   },
 
   async update(id, data) {
-    const res = await fetch(`${BASE_URL}/task/${id}`, {
+    const res = await fetch(`/task/${id}`, {
       method: 'PUT',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(data),
@@ -34,14 +34,14 @@ export const taskService = {
   },
 
   async delete(id) {
-    const res = await fetch(`${BASE_URL}/task/${id}`, {
+    const res = await fetch(`/task/${id}`, {
       method: 'DELETE'
     });
     if (!res.ok) throw new Error('Erro ao deletar tarefa');
   },
 
   async marcar(id) {
-    const res = await fetch(`${BASE_URL}/task/marcar/${id}`, {
+    const res = await fetch(`/task/marcar/${id}`, {
       method: 'PUT'
     });
     if (!res.ok) throw new Error('Erro ao marcar tarefa');
@@ -49,7 +49,7 @@ export const taskService = {
   },
 
   async search(vrpesq){
-    const res = await fetch(`${BASE_URL}/task/filtrar/${vrpesq}`,{
+    const res = await fetch(`/task/filtrar/${vrpesq}`,{
       method: 'GET'
     })
     if (!res.ok) throw new Error("Erro ao buscar tarefa")
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import 'dotenv/config'
 import express from "express"
 import cors from 'cors'
+import path from 'path'
 import tasksRoutes from "./src/routes/taskRoutes.js"
 
 const app = express()
@@ -8,6 +9,11 @@ app.use(cors())
 app.use(express.json())
 app.get('/', (req, res) => {
   res.send('API rodando 🚀')
+})
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.sendFile(process.cwd() + '/public/index.html')
 })
 app.use(tasksRoutes)
 
